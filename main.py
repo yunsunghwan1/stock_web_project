d5f9994662e536df437de1f8dad2494ea468e275
yunsunghwan <ban05088@naver.com>
2022-08-02 03:08:23 +0900

    test

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-view = [1,2,3,4,]
+import streamlit as st
+
+view = [100, 78, 4, 30]
+
+st.write('# View List')
+
+st.write('## raw data')
 view
+
+st.write('## bar chart')
+st.bar_chart(view)
+
+st.write('## line chart')
+st.line_chart(view)
+
+
+topics = [
+    {'id':1, 'title':'html', 'view':100},
+    {'id':2, 'title':'css', 'view':50},
+    {'id':3, 'title':'js', 'view':140}
+]
+
